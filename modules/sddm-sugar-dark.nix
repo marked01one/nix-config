64483a196684f1b18048b37cfe02e3e066c91f0a
marked01one <minhkhoitran2k3@gmail.com>
2024-03-31 22:35:28 -0700

    generation

--- modules/sddm-sugar-dark.nix
+++ modules/sddm-sugar-dark.nix
@@ -4,7 +4,7 @@ let
 
   image = pkgs.fetchurl {
     url = imgLink;
-    sha256 = pkgs.lib.fakeSha256;
+    sha256 = "sha256-CS/VngzYRpzZA3oVJUTcCvh7dhtXMx8Qvf/cpCYesLc=";
   };
 in
 pkgs.stdenv.mkDerivation {
