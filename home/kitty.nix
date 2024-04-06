8567d9ee3511175c224645fe5b308baccce44a33
marked01one <minhkhoitran2k3@gmail.com>
2024-04-06 12:25:28 -0700

    generation

--- home/kitty.nix
+++ home/kitty.nix
@@ -34,18 +34,18 @@
       enableZshIntegration = true;
 
       settings = {
-    add_newline = false;
-    format = lib.concatStrings [
-      "$line_break"
-      "$package"
-      "$line_break"
-      "$character"
-    ];
-    scan_timeout = 10;
-    character = {
-      success_symbol = "➜";
-      error_symbol = "➜";
+      add_newline = false;
+      format = lib.concatStrings [
+        "$line_break"
+        "$package"
+        "$line_break"
+        "$character"
+      ];
+      scan_timeout = 10;
+      character = {
+        success_symbol = "➜";
+        error_symbol = "➜";
+      };
     };
-  }
   };
 }
