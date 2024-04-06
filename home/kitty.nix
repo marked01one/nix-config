ab3f697bd44cfbc86d0b4e9e2c0db085b349a2f9
marked01one <minhkhoitran2k3@gmail.com>
2024-04-06 12:23:45 -0700

    generation

--- home/kitty.nix
+++ home/kitty.nix
@@ -28,4 +28,24 @@
       "ctrl+shift+j" = "scroll_line_down";
     };
   };
+
+  programs.starship = {
+      enable = true;
+      enableZshIntegration = true;
+
+      settings = {
+    add_newline = false;
+    format = lib.concatStrings [
+      "$line_break"
+      "$package"
+      "$line_break"
+      "$character"
+    ];
+    scan_timeout = 10;
+    character = {
+      success_symbol = "➜";
+      error_symbol = "➜";
+    };
+  }
+  };
 }
---
+++ home/vscode.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+
+{
+    programs.vscode = {
+        enable = true;
+        extensions = with pkgs.vscode-extensions; [
+            jnoortheen.nix-ide
+            vscodevim.vim
+        ];
+
+        userSettings = {
+            "nix.enableLanguageServer" = true;
+            "nix.serverPath" = "nil";
+            "nix.serverSettings"."nil" = {
+                formatting.command = [ "alejandra" ];
+            };
+        };
+    };
+}
