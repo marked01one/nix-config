f480c8c252372121e4666ba729e15ce1526bfd6a
marked01one <minhkhoitran2k3@gmail.com>
2024-04-12 19:24:27 -0700

    generation

--- home/ags/components/applauncher.js
+++ home/ags/components/applauncher.js
@@ -39,7 +39,7 @@ const AppItem = app => Widget.Button({
 
 function AppLauncher() {
     // list of application buttons
-    let applications = query("").map(AppItem).filter(app => !app.name?.includes("KiCad "));
+    let applications = query("").map(AppItem)
 
     // container holding the buttons
     const list = Widget.Box({
@@ -50,7 +50,7 @@ function AppLauncher() {
 
     // repopulate the box, so the most frequent apps are on top of the list
     function repopulate() {
-        applications = query("").map(AppItem)
+        applications = query("").map(AppItem).filter(app => !app.name?.includes("KiCad "))
         list.children = applications
     }
 
