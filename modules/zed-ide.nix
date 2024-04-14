5240b84bb4fdf79f8ce722270297beec987c776c
marked01one <minhkhoitran2k3@gmail.com>
2024-04-13 18:56:09 -0700

    generation

--- modules/zed-ide.nix
+++ modules/zed-ide.nix
@@ -3,9 +3,8 @@
 pkgs.stdenv.mkDerivation {
   name = "zed-ide";
 
-  src = pkgs.fetchFromGitHub {
-    owner = "zed-industries";
-    repo = "zed";
+  src = fetchGit {
+    url = "https://github.com/zed-industries/zed.git";
     rev = "3289188e0adaa6c6e73836f8beef5abad3982376";
     sha256 = "7xJ/RGLtVF+crdZYcuEJxob2z6+znQNqzc1QeczNoqo=";
   };
@@ -23,6 +22,8 @@ pkgs.stdenv.mkDerivation {
   ];
 
   buildPhase = ''
+    mkdir -p $out
+    cp -R ./*
     cd $out
     git submodule update --init --recursive
     cargo run --release
