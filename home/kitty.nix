57e14811603641343706b25e407e32446d231054
marked01one <minhkhoitran2k3@gmail.com>
2024-04-15 22:06:44 -0700

    generation

--- home/kitty.nix
+++ home/kitty.nix
@@ -27,8 +27,8 @@
     settings = {
       confirm_os_window_close = 0;
       dynamic_background_opacity = true;
-      background_opacity = "0.75";
-      background_blur = 3;
+      background_opacity = "0.8";
+      background_blur = 10;
     };
   };
 
