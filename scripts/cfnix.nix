fc4c72f66453746da7dc17fbe886ca7ee8b2141a
marked01one <minhkhoitran2k3@gmail.com>
2024-04-17 13:42:10 -0700

    generation

--- scripts/cfnix.nix
+++ scripts/cfnix.nix
@@ -1,9 +1,6 @@
 {pkgs}:
 
 pkgs.writeShellScriptBin "check-pkg" ''
-  RESULT=$(nix build nixpkgs#$1 --print-out-paths --no-link | cd)
-  echo "$RESULT"
-  if [ $exit_code == 0 ]; then
-    nix run nixpkgs#lsd -- --tree --depth 4
-  fi
+  cd "$(nix build nixpkgs#$1 --print-out-paths --no-link)" 
+  nix run nixpkgs#lsd -- --tree --depth 4
 ''
