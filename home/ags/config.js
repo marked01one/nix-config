6be4ef5fd81f8474724a4bdccf0d06a3c1c3d93f
marked01one <minhkhoitran2k3@gmail.com>
2024-04-18 20:52:03 -0700

    generation

--- home/ags/config.js
+++ home/ags/config.js
@@ -1,8 +1,10 @@
 import { bluetooth } from "./components/bluetooth.js";
 import { applauncher } from "./components/applauncher.js";
+import { taskbar } from "components/taskbar.js";
 
 App.config({
     windows: [
         applauncher,
+        taskbar()
     ],
 })
