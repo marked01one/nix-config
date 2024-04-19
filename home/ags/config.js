2a01315a635aaea696e4b3f6edfc63dffb5aad75
marked01one <minhkhoitran2k3@gmail.com>
2024-04-18 21:25:19 -0700

    generation

--- home/ags/config.js
+++ home/ags/config.js
@@ -1,5 +1,5 @@
 import { applauncher } from "./components/applauncher.js";
-import { taskbar } from "components/taskbar.js";
+import { taskbar } from "./components/taskbar.js";
 
 App.config({
     style: "./styles.css",
