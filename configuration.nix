97a359cecbd94a121f648f65b577e8f175343675
marked01one <minhkhoitran2k3@gmail.com>
2024-04-18 18:24:00 -0700

    generation

--- configuration.nix
+++ configuration.nix
@@ -313,31 +313,6 @@ in {
       ];
     };
 
-    zsh = {
-      enable = true;
-      enableCompletion = true;
-      autosuggestions.enable = true;
-      syntaxHighlighting.enable = true;
-
-      shellAliases = {
-        # Allow `sudo` to be used with aliases
-        sudo = "sudo ";
-        ls = "lsd -1";
-        ll = "lsd -1 -a -l";
-        c = "/run/current-system/sw/bin/code";
-
-        cfnix = "/run/current-system/sw/bin/code ~/SystemFlake";
-        update = "sudo nixos-rebuild switch --flake ~/SystemFlake/#perfect-linux";
-
-        logout = "pkill -u $(whoami)";
-        shutdown = "systemctl poweroff";
-        "copy-pw" = "sudo cat ~/.token | xclip -sel clip";
-
-        home = "cd ~";
-        ".." = "cd ..";
-        "..." = "cd ../..";
-      };
-    };
     hyprland = {
       enable = true;
 
--- home/zsh.nix
+++ home/zsh.nix
@@ -1,5 +1,37 @@
 {pkgs, ...}: {
   programs.zsh = {
     enable = true;
+
+    enableCompletion = true;
+    autosuggestions = {
+      enable = true;
+      async = true;
+      strategy = "completion";
+    };
+    syntaxHighlighting = {
+      enable = true;
+      package = pkgs.zsh-syntax-highlighting; 
+    };
+
+    shellAliases = {
+        # Allow `sudo` to be used with aliases
+        sudo = "sudo ";
+        ls = "lsd -1";
+        ll = "lsd -1 -a -l";
+        c = "/run/current-system/sw/bin/code";
+
+        cfnix = "/run/current-system/sw/bin/code ~/SystemFlake";
+        update = "sudo nixos-rebuild switch --flake ~/SystemFlake/#perfect-linux";
+
+        logout = "pkill -u $(whoami)";
+        shutdown = "systemctl poweroff";
+        "copy-pw" = "sudo cat ~/.token | xclip -sel clip";
+
+        home = "cd ~";
+        ".." = "cd ..";
+        "..." = "cd ../..";
+      };
+    
+
   };
 }
