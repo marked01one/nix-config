859e971f504677825368e57b1f4deafab248e16c
marked01one <minhkhoitran2k3@gmail.com>
2024-04-22 18:08:31 -0700

    generation

--- system/scripts/swww-init.nix
+++ system/scripts/swww-init.nix
@@ -1,8 +1,8 @@
 {pkgs}:
 
 pkgs.writeShellScriptBin "swww-init" ''
-  numOfWallpapers = $(find ~/Pictures/Wallpapers/ -maxdepth 1 -type f | wc -l)
-  randNum = $((1 + $RANDOM % $numOfWallpapers))
+  numOfWallpapers=$(ls -lR $HOME/Pictures/Wallpapers | grep '^l' | wc -l)
+  randNum=$((1 + $RANDOM % $numOfWallpapers))
   swww-daemon &
   swww img ~/Pictures/Wallpapers/$randNum.* &
   nm-applet --indicator
