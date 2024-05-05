e80d5e3d4e0b1e56777fdd4ae0fd6fde08a7228f
marked01one <minhkhoitran2k3@gmail.com>
2024-05-05 16:44:47 -0700

    generation

--- configuration.nix
+++ configuration.nix
@@ -306,7 +306,7 @@ in {
 
   # Enable sound with pipewire.
   sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
