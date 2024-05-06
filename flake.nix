bc6a44d34445e70d9c3d1aea51085017108d6870
marked01one <minhkhoitran2k3@gmail.com>
2024-05-06 02:05:32 -0700

    generation

--- flake.nix
+++ flake.nix
@@ -4,10 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     hyprcursor.url = "github:hyprwm/hyprcursor";
-    # hyprland = {
-    #   url = "github:hyprwm/Hyprland";
-    #   inputs.hyprland.follows = "nixpkgs";
-    # };
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.hyprland.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
