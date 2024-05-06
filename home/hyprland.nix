b9ee1e1b95091f09e36514745807a216b703d4ad
marked01one <minhkhoitran2k3@gmail.com>
2024-05-05 21:32:23 -0700

    generation

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -174,7 +174,7 @@ in {
       };
 
       plugins = [
-        inputs.hyprland-plugins.packages.${pkgs.system}.hyprexpo
+        inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
       ];
     };
   };
