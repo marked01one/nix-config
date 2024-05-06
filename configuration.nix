438d2f214a738ddefd46e1ea3a7dd9697d410281
marked01one <minhkhoitran2k3@gmail.com>
2024-05-06 10:57:00 -0700

    generation

--- configuration.nix
+++ configuration.nix
@@ -405,16 +405,10 @@ in {
 
   nix = {
     settings = {
-      auto-optimise-store = true;
       experimental-features = ["nix-command" "flakes"];
     };
 
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
   };
 
-  system.stateVersion = "unstable"; 
+  system.stateVersion = "24.05"; 
 }
