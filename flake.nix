de28a23444a43c3f7fcc9f8b3b149190ee3f755f
marked01one <minhkhoitran2k3@gmail.com>
2024-05-06 01:07:04 -0700

    generation

--- flake.nix
+++ flake.nix
@@ -45,9 +45,6 @@
     homeConfigurations = {
       "marked01one@perfect-linux" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages."x86_64-linux";
-        extraSpecialArgs = {
-          inherit inputs;
-        };
         modules = [./home.nix];
       };
     };
