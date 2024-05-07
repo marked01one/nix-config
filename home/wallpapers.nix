167dc517d38d60e8c24d225dec1099c468c69337
marked01one <minhkhoitran2k3@gmail.com>
2024-05-07 14:36:10 -0700

    generation

--- home/wallpapers.nix
+++ home/wallpapers.nix
@@ -17,12 +17,17 @@
 
     "Pictures/Wallpapers/4.jpg".source = pkgs.fetchurl {
       url = "https://64.media.tumblr.com/b5840353876cea0a633f58c0d64cd17f/9eff55e60be51079-3c/s2048x3072/85fa6c185973bb21e0379c3e09929531f6218e07.jpg";
-      sha256 = pkgs.lib.fakeSha256;
+      sha256 = "ECrsHX19D7xbZp1xr5a62OHz6dACrmMzr1tzXWkdeW0=";
     };
 
     "Pictures/Wallpapers/5.jpg".source = pkgs.fetchurl {
       url = "https://64.media.tumblr.com/bcad9fd013371232246f4ff7c9a21ef4/7acd94cc2a64cb3d-50/s2048x3072/1198a7d14699092b741702e96b47d8e839d560b3.jpg";
       sha256 = "YNswouEqYXheOF/NARX7hLsReMoQkjpYMHeJkXAj1Ag=";
+    };
+
+    "Pictures/Wallpapers/6.jpg".source = pkgs.fetchurl {
+      url = "https://64.media.tumblr.com/718910e42e79880635309ece84ad3f33/54725b5271a1eae6-30/s2048x3072/0696ab053573d3c7b1f43a8ad9aa9d2c55bb38e5.jpg";
+      sha256 = pkgs.lib.fakeSha256;
     };
   };
 }
