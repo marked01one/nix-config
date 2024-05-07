8959b60a352b9228793d5a6616c2765c9b51b5ff
marked01one <minhkhoitran2k3@gmail.com>
2024-05-06 19:57:44 -0700

    generation

--- system/scripts/swww-init.nix
+++ system/scripts/swww-init.nix
@@ -3,7 +3,7 @@ pkgs.writeShellScriptBin "swww-init" ''
   numOfWallpapers=$(ls -lR $HOME/Pictures/Wallpapers | grep '^l' | wc -l)
   randNum=$((1 + $RANDOM % $numOfWallpapers))
   swww-daemon &
-  swww img ~/Pictures/Wallpapers/$randNum.* &
+  swww img ~/Pictures/Wallpapers/$randNum.* --transition-step=15 --transition-fps=240 --transition-type=wipe --transition-angle=0 --transition-duration=1 &
   nm-applet --indicator &
   waybar &
 ''
