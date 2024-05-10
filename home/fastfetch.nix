1a88569ffa49f861d4cbb0d7d025729b7858a6ad
marked01one <minhkhoitran2k3@gmail.com>
2024-05-09 23:13:35 -0700

    generation

--- home/fastfetch.nix
+++ home/fastfetch.nix
@@ -1,5 +1,10 @@
 {pkgs,...}: {
   home.file = {
     ".config/fastfetch/config.jsonc".source = ./../config/fastfetch/fastfetch.jsonc;
+
+    ".config/fastfetch/logo1.jpg".source = pkgs.fetchurl {
+      url = "https://64.media.tumblr.com/16958cb609849796becc3ce87d321228/8adc7574e99f1e43-e6/s1280x1920/6153745aed37443a760e04689d4a8561a3a76354.jpg";
+      sha256 = pkgs.lib.fakeSha256;
+    };
   };
 }
