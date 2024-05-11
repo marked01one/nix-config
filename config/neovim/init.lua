aa76c71e46ee5ce1ba6be32026481a30248ba377
marked01one <minhkhoitran2k3@gmail.com>
2024-05-11 15:33:54 -0700

    generation

--- config/neovim/init.lua
+++ config/neovim/init.lua
@@ -4,3 +4,20 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 
 
+-- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- if not vim.loop.fs_stat(lazypath) then
+--     vim.fn.system({
+--         "git",
+--         "clone",
+--         "--filter=blob:none",
+--         "https://github.com/folke/lazy.nvim.git",
+--         "--branch-stable",
+--         lazypath
+--     })
+-- end
+-- vim.opt.rtp:prepend(lazypath)
+
+local plugins = {}
+local options = {}
+
+require("lazy").setup(plugins, options)
