a9c905cf43cb562a3df01845e84fd99d5f4780cf
marked01one <minhkhoitran2k3@gmail.com>
2024-05-11 18:14:19 -0700

    generation

--- config/neovim/init.lua
+++ config/neovim/init.lua
@@ -3,21 +3,12 @@ vim.cmd("set shiftwidth=4")
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 
-
--- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- if not vim.loop.fs_stat(lazypath) then
---     vim.fn.system({
---         "git",
---         "clone",
---         "--filter=blob:none",
---         "https://github.com/folke/lazy.nvim.git",
---         "--branch-stable",
---         lazypath
---     })
--- end
--- vim.opt.rtp:prepend(lazypath)
-
-local plugins = {}
+local plugins = {
+   {"catpuccin/nvim", name = "catpuccin", priority = 1000} 
+}
 local options = {}
 
 require("lazy").setup(plugins, options)
+
+require("catpuccin").setup()
+vim.cmd.colorscheme "catpuccin"
