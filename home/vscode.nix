f65759094fec67e74dcbd293d22103381b1d1f2f
marked01one <minhkhoitran2k3@gmail.com>
2024-05-13 14:26:34 -0700

    generation

--- home/vscode.nix
+++ home/vscode.nix
@@ -14,6 +14,7 @@
         enkia.tokyo-night
         golang.go
         sumneko.lua
+        postman.postman-for-vscode
       ]
       ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
         {
