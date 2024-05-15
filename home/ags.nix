c3cfcf0b1125e80d3c68a617c8d7b4f6b52707fe
marked01one <minhkhoitran2k3@gmail.com>
2024-05-15 15:54:48 -0700

    generation

--- home/ags.nix
+++ home/ags.nix
@@ -11,10 +11,13 @@
 
   home.file = {
     ".config/ags/config.js".source = ./../config/ags/config.js;
-    ".config/ags/styles.css".source = ./../config/ags/styles.css;
     ".config/ags/tsconfig.json".source = ./../config/ags/tsconfig.json;
     ".config/ags/components" = {
       source = ./../config/ags/components;
+      recursive = true;
+    };
+    ".config/ags/styles" = {
+      source = ./../config/ags/styles;
       recursive = true;
     };
   };
