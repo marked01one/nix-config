9964c120fbdebe3396f5e4702e4c9b1db0b41eb5
marked01one <minhkhoitran2k3@gmail.com>
2024-05-16 16:09:49 -0700

    generation

--- config/ags/config.js
+++ config/ags/config.js
@@ -1,12 +1,15 @@
 import { applauncher } from "./components/applauncher.js";
 import { Taskbar } from "./components/taskbar.js";
+const hyprland = await Service.import("hyprland")
+
+let taskbar = hyprland.monitors.map(m => Taskbar(m.id))
 
 App.config({
     style: "./styles/taskbar.css",
     windows: [
         applauncher,
-        Taskbar()
-    ],
+        taskbar
+    ].flat(),
 })
 
 export {  }
