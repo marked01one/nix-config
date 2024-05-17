a33c3ea72e6bef7cbc31a8cb020e18411de45b1a
marked01one <minhkhoitran2k3@gmail.com>
2024-05-16 18:00:19 -0700

    generation

--- config/ags/components/taskbar.js
+++ config/ags/components/taskbar.js
@@ -14,8 +14,9 @@ const date = Variable("", {
 function Workspaces() {
     const activeId = hyprland.active.workspace.bind("id")
     const workspaces = hyprland.bind("workspaces")
-        .as(ws => ws.map(
-            ({ id }) => Widget.Button({
+        .as(ws => ws
+            .sort((wsa, wsb) => wsa.id - wsb.id)
+            .map(({ id }) => Widget.Button({
                 on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
                 child: Widget.Label(`${id}`),
                 class_name: activeId.as(i => `${i === id ? "focused" : ""}`),
