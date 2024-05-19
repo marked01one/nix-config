5046c2085dff4464bf1c55f70e358884fc32a051
marked01one <minhkhoitran2k3@gmail.com>
2024-05-19 10:03:29 -0700

    generation

--- configuration.nix
+++ configuration.nix
@@ -288,7 +288,7 @@ in {
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
         CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 20;
+        CPU_MAX_PERF_ON_BAT = 50;
       };
     };
     blueman.enable = true;
