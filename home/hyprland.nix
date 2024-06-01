a57f9aac67652d47caf50505f1f9496690e24154
marked01one <minhkhoitran2k3@gmail.com>
2024-05-31 19:40:16 -0700

    generation

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -46,17 +46,17 @@ in {
         "EGL_PLATFORM,wayland"
         "ELECTRON_OZONE_PLATFORM_HINT,auto"
 
-        "MOZ_ENABLE_WAYLAND,1"
-        "MOZ_WAYLAND_USE_VAAPI,1"
-        "MOZ_DBUS_REMOTE,1"
-        "MOZ_ACCELERATED,1"
-        "MOZ_WEBRENDERER,1"
-        "MOZ_DISABLE_RDD_SANDBOX,1"
+        # "MOZ_ENABLE_WAYLAND,1"
+        # "MOZ_WAYLAND_USE_VAAPI,1"
+        # "MOZ_DBUS_REMOTE,1"
+        # "MOZ_ACCELERATED,1"
+        # "MOZ_WEBRENDERER,1"
+        # "MOZ_DISABLE_RDD_SANDBOX,1"
         
-        "_JAVA_AWT_WM_NONREPARENING,1"
-        "WLR_USE_LIBINPUT,1"
-        "WLR_DRM_NO_ATOMIC,1"
-        "__GL_VRR_ALLOWED,1"
+        # "_JAVA_AWT_WM_NONREPARENTING,1"
+        # "WLR_USE_LIBINPUT,1"
+        # "WLR_DRM_NO_ATOMIC,1"
+        # "__GL_VRR_ALLOWED,1"
         "__GLX_VENDOR_LIBRARY_NAME,nvidia"
       ];
 
