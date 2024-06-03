7944a7b03da5d2ff923ef154b38a04d68799954b
marked01one <minhkhoitran2k3@gmail.com>
2024-06-02 20:00:33 -0700

    generation

--- configuration.nix
+++ configuration.nix
@@ -163,13 +163,14 @@ in {
   nixpkgs.overlays = [
     (final: prev: {
       hyprland = prev.hyprland.overrideAttrs (old: {
-        src = prev.fetchFromGitHub {
-          owner = "hyprwm";
-          repo = "Hyprland";
-          fetchSubmodules = true;
-          rev = "0.39.1";
-          hash = "sha256-Urb/njWiHYUudXpmK8EKl9Z58esTIG0PxXw5LuM2r5g=";
-        };
+        version = "0.39.1";
+        # src = prev.fetchFromGitHub {
+        #   owner = "hyprwm";
+        #   repo = "Hyprland";
+        #   fetchSubmodules = true;
+        #   rev = "0.39.1";
+        #   hash = "sha256-Urb/njWiHYUudXpmK8EKl9Z58esTIG0PxXw5LuM2r5g=";
+        # };
       });
     })
   ];
