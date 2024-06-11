f815e4b8e5479b6e95bcd770c2ea93c97706372d
marked01one <minhkhoitran2k3@gmail.com>
2024-06-11 12:41:02 -0700

    updated the backlight cycling service

--- services/googler/main.go
+++ services/googler/main.go
@@ -4,17 +4,35 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"strconv"
 )
 
 var modes = []string{"off", "low", "medium", "high"}
 
-func main() {
-	backlightDir := "/sys/class/leds/asus::kbd_backlight/brightness"
-
-	out, err := exec.Command(fmt.Sprintf("echo 1 > %s", backlightDir)).Output()
+func logError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	return
+}
 
-	fmt.Printf("Changed backlight brightness to %s\n", modes[1])
+func main() {
+	backlightDir := "/sys/class/leds/asus::kbd_backlight/brightness"
+
+	currBright, err := exec.Command("sh", "-c", fmt.Sprintf("cat %s", backlightDir)).Output()
+	logError(err)
+
+	currValue, err := strconv.ParseInt(string(currBright[:1]), 10, 32)
+	logError(err)
+
+	var nextValue int64 = 0
+
+	if currValue < 3 {
+		nextValue = currValue + 1
+	}
+
+	_, err = exec.Command("sh", "-c", fmt.Sprintf("echo %d > %s", nextValue, backlightDir)).Output()
+	logError(err)
+
+	fmt.Printf("Changed backlight brightness to %s\n", modes[nextValue])
 }
